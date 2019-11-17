dcda3c199c49fd0f9d4c91b5684d7c9702de41e2
varinea <varinea.lopez@gmail.com>
2019-11-17 20:05:50 -0300

    luhn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import validator from './validator.js';
 
-console.log(validator);
+
+function getUserInput(){
+    return userNumInput.value;  
+ }
+ 
+ function luhnCheck(){
+   return "hello world";
+ }
+ 
+ function whatCard(){
+ 
+ }
+ 
+ document.getElementById("submitBtn").addEventListener("click", function(){
+    document.getElementById("resultDiv").innerHTML = luhnCheck();
+ }, false);
--- src/validator.js
+++ src/validator.js
@@ -1,47 +1,70 @@
-//Creamos funcion isValidCard:
-function cardValidation(number){
-  
-  //Creamos un array vacio:
-  arr=[];
+var userNumInput = document.getElementById("userNum"); 
 
-  //Creamos un bucle que recorra number:
-  for (var i = 0; i < number.length ; i++) {
-    
-    //Con esto vamos agregando los elementos al array vacio:    
-    arr.push(number[i]);
+function getUserInput(){ 
+   return userNumInput.value;   //Con input el tomo el valor que el usuario me da
+}
+
+//una vez obtenido el la númeracion de la tarjeta de credito.
+function luhnCheck(){  //Aqui veremos si la información entregada es real, asi que se crea un boolano
+  var ccNum = getUserInput() //Se toma la numeracion de la CreditCard
+  var ccNumSplit = ccNum.split("") //con split estamos dividiendo para convertirlo en una serie de cadenas
+  var sum = 0;  // sumamos
+  var singleNums = [] //array vacia
+  var doubleNums = []  //doble array vacia
+  var finalArry = undefined; //
+  var validCard = false;
+  
+  //Ahora paso a verificar si hay datos validos utilizando if
+
+  if((!/\d{15,16}/g.test(ccNum)) || (ccNum.length > 16)){
+  //  \d{15,16}/  aqui quiero un digito de 15,16 de longitud  
+  // g hace que todo sea global y para eso necesito test  para probar ccNum || (o) ccNum quiero que su longitud sea mayor a 16
+  //Con la expresion  !   estoy revertir la logica, si no tengo numeros se rechazara.
+   //   busque estas expresiones en  https://regex101.com/
+     return false;  
   }
 
-  //Los elementos del array estaran alreves:
-  var reverse = arr.reverse();
-  var sum = 0; //Inicializamos sum en 0
-  var double = 0; //Inicializamos double en 0
-
-  //Creamos un bucle que recorra el array, ya al reves:
-  for (var j = 1; j < reverse.length-2 ; j+=2){
-    
-    /*Condicional para saber si los elementos, con indice pares, 
-    multiplicados por 2 son mayor a 9: */
-    if(reverse[j] * 2 > 9){
-
-      sum += parseInt((reverse[j] * 2) / 10) + (reverse[j] * 2) % 10; //Se suman las cifras de el numero y se van acumulando
-
-    }else{
-      double += reverse[j] * 2 //Se van acumulando los que multiplicados por 2 no son mayor que 9
-    }
+  if(ccNum.length === 15){  //american express 
+     for(var i = ccNumSplit.length-1; i>=0; i--){  //en for coloco -- para que comienze por la derecha 
+        if(i % 2 === 0){ //
+           singleNums.push(ccNumSplit[i]);
+        }else{
+           doubleNums.push((ccNumSplit[i] * 2).toString());
+        }
+     }
+  }else if(ccNum.length === 16){
+     for(var i = ccNumSplit.length-1; i>=0; i--){
+        if(i % 2 !== 0){
+           singleNums.push(ccNumSplit[i]);
+        }else{
+           doubleNums.push((ccNumSplit[i] * 2).toString());
+        }
+     }
+  }
+  //joining makes an array to a string and I split them up again
+  //so that every number is a single digit and convert back to array
+  
+  doubleNums = doubleNums.join("").split("");  
+  finalArry = doubleNums.concat(singleNums);
+  
+  for(var j = 0; j<finalArry.length; j++){
+     sum += parseInt(finalArry[j]);
   }
   
-  //Cuando se llame la función, retornará la suma de todos los numeros:
-  return (double + sum) + reverse[j-1];
-
+  if(sum % 10 === 0){
+     validCard = true;
+  }
+  //the console log is for you, so you can see the sum, all sums that are
+  //divisible by 10 should be good.  Just open up your console to view.
+  
+  console.log(sum);
+  return validCard;
 }
 
-//Llamamos a la funcion y condicionamos para saber si es una tarjeta valida:
-if (cardValidation(prompt('Ingrese numero de Tarjeta')) % 10 == 0){
-
-    alert('Tarjeta Valida');
+function whatCard(){
+   
 }
 
-else{
-
-    alert('Tarjeta Invalida')
-}
+document.getElementById("submitBtn").addEventListener("click", function(){ //Evento DOM Aqui se le da la respuesta al usuario despues de que haya apretado el boton para saber la respuesta
+   document.getElementById("resultDiv").innerHTML = luhnCheck();
+}, false);
